e12b341ff84d282d96ba1161b39b0b108e30cf3d
aron64 <aronhertendi@gmail.com>
2019-09-22 21:17:06 +0200

    Added 2 classes to handle communication and switching between interfaces

---
+++ client/init.py
@@ -0,0 +1,121 @@
+#!python3
+"""
+Main root for the client, managing switching in-between the different interfaces of the client: 
+		login - read - write
+Missing commincation with the server.
+"""
+from tkinter import *
+import tkinter.ttk as ttk
+from tkinter import messagebox
+import socket,threading,time,os,getpass
+from datetime import datetime as dt
+
+from client_gui import EmailGui, WriteLetterDialog, AuthenticationDialog, ShowLetter
+
+class EmailClient(Frame):
+	"""docstring for EmailClient"""
+	def __init__(self, host, port, name):
+		Frame.__init__(self)
+		self.master.resizable(0,0)
+		self.master.protocol("WM_DELETE_WINDOW", self._delete_window)
+		self.center_window()
+		#self.master.attributes('-fullscreen', True)
+
+		# Widgets #
+		self.auth = AuthenticationDialog(self)
+		
+		#online mode
+		self.data = [StringVar(),StringVar(),StringVar()]
+		self.data[0].set(host)
+		self.data[1].set(port)
+		self.data[2].set(name)
+		self.connect()
+		if self.connected:
+			self.c=Commincation(self.connection,self)
+			self.c.start()
+
+		#start
+
+		self.auth.grid()
+		self.grid()
+
+		self.master.title('Login or register')
+
+
+	def _delete_window(self):
+		"function to close active connections before exiting"
+		try:
+			self.connection.send(bytes('#fin#', 'utf-8'))
+		except Exception as e:
+			pass
+		finally:
+			self.master.destroy()
+
+	def center_window(self):
+		self.master.update_idletasks()
+		self.master.geometry("+%d+%d" % (self.master.winfo_screenwidth()/2-(self.master.winfo_reqwidth()/2),
+                                  self.master.winfo_screenheight()/2-(self.master.winfo_reqheight()/2)-75))
+
+
+	def login_success(self, user):
+		self.master.title('Email-client')
+		self.auth.grid_forget()
+		self.user = user
+		self.client = EmailGui(self, user)
+		self.client.grid()
+		self.center_window()
+
+	def logout(self):
+		self.auth.grid()
+		self.master.title('Login or register')
+		self.center_window()
+		
+	def write_letter(self):
+		self.client.grid_forget()
+		self.master.title('Send new message')
+		self.send = WriteLetterDialog(self, self.user)
+		self.send.grid()
+		self.center_window()
+
+	def send_over(self):
+		self.master.title('Email-client')
+		self.client.grid()
+		self.center_window()
+
+	def connect(self):
+		self.connection=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		print("Csatlakozás - IPv4: {0}, Port: {1}".format(self.data[0].get(),self.data[1].get()))
+		HOST, PORT=self.data[0].get(),int(self.data[1].get())
+		try:
+			self.connection.connect((HOST, PORT))
+			print("A kapcsolat létrejött.")
+			self.connection.send(bytes(self.data[2].get(), 'utf-8'))
+			self.data[2].set(self.connection.recv(1024).decode('utf-8'))  #may receive a unique id
+			self.connected=True
+		except socket.error as e:
+			print(str(e))
+			print("A kapcsolat meghiúsult")
+			self.connected=False
+			return
+
+class Commincation(threading.Thread):
+	"""docstring for Commincation"""
+	def __init__(self, conn, root):
+		threading.Thread.__init__(self)
+		self.conn = conn
+		self.root = root
+
+	def run(self):
+		while 1:
+			if self.root.connected:
+				self.conn.send(bytes("a", 'utf-8'))
+				sleep(60)
+			else:break
+		
+
+if __name__ == '__main__':
+	host = socket.gethostname()  # Get local machine name
+	port = 50000  # Reserve a port for your service.
+	name = os.environ['COMPUTERNAME']+'\\'+getpass.getuser() # log in with username
+	root=EmailClient(host, port, name)
+	root.mainloop()
