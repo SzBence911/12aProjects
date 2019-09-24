abc834ae8c3bd8261506fd76b1f287ee1379dc6e
Szita Bence <48180569+SzBence911@users.noreply.github.com>
2019-09-24 19:53:59 +0200

    Added timer to kick AFK

--- server/init.py
+++ server/init.py
@@ -2,8 +2,10 @@ from crypting import *
 from socket import *
 import threading
 import datetime
+import time
 
 connected = {}
+idtimer = {}
 
 def newcon(c, addr,con):    #get connection type
     data = c.recv(1024)
@@ -12,18 +14,38 @@ def newcon(c, addr,con):    #get connection type
     elif data.split(';', 0) == "l":
         loggingin(data,c)
 
-def send(con, mail): #If user want to send mail
-    mail = mail
+def send(c): #If user want to send mail
+    c.send("1")
+    out = 0
+    while True:
+        data = c.recv(2048) #256 char buffer
+        if data is not None:
+            store(data)
+        else:
+            out += 1
+        if out > 10:
+            break
 
-
-def receive(con): #If user have new mail and he logged in
+def receive(c): #If user have new mail and he logged in
     store(con)
 
-def store(con): #Stone emails
+def store(data): #Store emails
     print("sucess")
+    return True
 
 def logged(id, name,c): #Pin  user to connected dictionary
-    connected[id] = name
+    if id in connected.keys():
+        while True:
+            try:
+                data = c.recv(40)
+            except:
+                data = False
+            if data != False:
+                data.split(';', 1)
+                if data[1] == "s":
+                    send(c)
+                elif data[1] == "r":
+                    receive(c)
 
 def registerin(data,c): #Register user to the system using DB
     data = data.split(';', 2)
@@ -41,12 +63,24 @@ def loggingin(data,c):    #Get datas from DB(Is user existed?)
             else:
                 log(data[1] + " sikeresen bejelentkezett")
                 connected[count1] = data[1]
+                idtimer[count1] = 0
                 c.send(count1)
                 logged(count1, data[1],c)
 
 def log(s):  # logs
     with open("logs/" + datetime.datetime.now().strftime("%Y.%m.%d") + "-console-log.log",encoding="utf-8", mode="a") as f:
         f.write(s + "\n")
+
+def kicker():
+    while True:
+        time.sleep(1)
+        counter = 0
+        while counter < idtimer[-1]:
+            if counter in idtimer.keys():
+                idtimer[counter] += 1
+                if idtimer[counter] > 600:
+                    del idtimer[counter]
+                    del connected[counter]
 
 s = socket.socket()  # Create a socket object
 host = socket.gethostname()  # Get local machine name
@@ -55,6 +89,8 @@ s.bind((host, port))  # Bind to the port
 s.listen(5)  # Now wait for client connection.
 print(createdb("Users"))
 print(createtb("Users", "useraut"))
+kick = threading.Thread(target=kicker, args=())
+kick.start()
 while True: #Create thread to new connection
     c, addr = s.accept()
     con = threading.Thread(target=newcon, args=(c,addr,con))
