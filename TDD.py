0780f1cb0c23a9df9ba91fb4025ae021b81ff848
Szita Bence <48180569+SzBence911@users.noreply.github.com>
2019-09-29 11:23:43 +0200

    TDD with send and receive menthod

--- TDD.py
+++ TDD.py
@@ -1,8 +1,36 @@
 import socket
+import time
 
 s = socket.socket()
 
 sip = ('localhost', 50000)
 s.connect(sip)
 
-s.send(b';l;asd;asd;')
+
+
+def  reg():
+    s.send(b';r;asd;asd;')
+
+def login():
+    s.send(b';l;asd;asd;')
+    sendmes(b";asdasdasdasd;")
+
+def sendmes(msg):
+    time.sleep(1)
+    s.send(b';s;ms')
+    if receive() == True:
+        s.send(msg)
+
+def receive():
+    dat = "b''"
+    while str(dat) == "b''":
+        dat = s.recv(32)
+        dat = str(dat)
+        dat = dat.split(';')
+        print(dat)
+        if dat[1] == "c":
+            return True
+        else:
+            return False
+
+login()
